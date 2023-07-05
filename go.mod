6d15f36d32e95fa220d75efd23aa0d378ad19b57
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-07-05 08:12:42 -0500

    feat: Parse arguments for git and shell usage

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bpjordan/multigit
 
 go 1.20
+
+require github.com/hellflame/argparse v1.11.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,32 @@
-
 package main
 
+import (
+	"strings"
+
+	"github.com/hellflame/argparse"
+)
+
 func main() {
-    println("Hello world")
+    parser := argparse.NewParser("mg", "Utility to manage several git repositories simultaneously", &argparse.ParserConfig{
+	WithColor: true,
+	WithHint: true,
+    })
+
+    git_pass := parser.AddCommand("git", "Run an arbitrary git command in all repositories", &argparse.ParserConfig{})
+
+    git_args := git_pass.Strings("a", "", &argparse.Option{Positional: true, Help: "Git arguments", Required: true})
+
+    sh_pass := parser.AddCommand("sh", "Run an arbitrary command in all repositories", &argparse.ParserConfig{})
+
+    sh_args := sh_pass.Strings("a", "", &argparse.Option{Positional: true, Help: "Shell command", Required: true})
+
+    parser.Parse(nil)
+
+    if git_args != nil {
+	println("Git args: ", strings.Join(*git_args, ", "))
+    }
+
+    if sh_args != nil {
+	println("Shell args: ", strings.Join(*sh_args, ", "))
+    }
 }
